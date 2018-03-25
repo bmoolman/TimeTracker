bfbc02ff56eea3ff1b0f56ed2f3a9fa50589d626
bmoolman <bruce.moolman@se.abb.com>
2018-03-25 10:25:35 +0200

    Add SQLite test code

--- TimeTracker/TimeTracker/Program.cs
+++ TimeTracker/TimeTracker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace TimeTracker
 {
@@ -10,7 +11,12 @@ namespace TimeTracker
     {
         static void Main(string[] args)
         {
-           
+            //Console.WriteLine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            SQLiteConnection sqlite_conn =
+                new SQLiteConnection("Data Source=database.sqlite;Version=3;");
+
+            sqlite_conn.Open();
+            sqlite_conn.Close();
 
         }
     }
